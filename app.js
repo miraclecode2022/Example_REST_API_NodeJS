c98473771f8b80e2f6693299740163eca1df0a9f
tamkiller <tamkiller1999@gmail.com>
2019-03-05 15:37:04 +0700

    aaa

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header(
         'Access-Control-Allow-Headers' , 
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-TOKEN'
     )
     if (req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
